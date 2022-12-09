24878e06dee6bf9f19a321f1f2579e3b9c40f7d2
Владислав Ежов <v.ezhov@qsoft.ru>
2022-12-09 10:36:18 +0300

    create page for final round CTF

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,120 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container text-center">
+    <h1>CTF</h1>
+    <div v-if="(table != null)" class="row align-items-start pb-3 section">
+      <div class="col">
+        {{ teams[0].name}}
+      </div>
+      <div class="col">
+        Задания
+      </div>
+      <div class="col">
+        {{ teams[1].name}}
+      </div>
+    </div>
+
+    <div v-if="(table != null)">
+      <div v-for="item in table" :key="item.name" class="row align-items-center pb-3 section">
+        <div class="col">
+          <img :src="item[0].photo" class="img-thumbnail" :alt="item[0].name">
+          {{ item[0].last_name }} {{ item[0].name }}
+        </div>
+        <div class="col">
+          {{ item[1].name }}
+        </div>
+        <div class="col">
+          <img :src="item[2].photo" class="img-thumbnail" :alt="item[2].name">
+          {{ item[2].last_name }} {{ item[2].name }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import _ from 'lodash';
+import Pusher from 'pusher-js';
+import { app } from './main.js'
+
+const axios = require('axios').default;
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      members: [],
+      tasks: [],
+      teams: [],
+    }
+  },
+  created() {
+    axios.get('http://37.230.113.138/api/data')
+      .then(response => {
+        this.table = response.data;
+      });
+    this.subscribeToBack();
+  },
+  computed: {
+    table: {
+      cache: false,
+      get() {
+        if (this.members.length == 0 || this.tasks == 0 || this.teams == 0 ) {
+          return null;
+        }
+
+        var members1 = [];
+        var members2 = [];
+
+        _.each(this.members, function(member) {
+          if (member.team_id == 1) {
+            members1.push(member);
+          }
+
+          if (member.team_id == 2) {
+            members2.push(member);
+          }
+        });
+
+        _.sortBy(this.tasks, 'id');
+        _.sortBy(members1, 'task_id');
+        _.sortBy(members2, 'task_id');
+
+        var data = [];
+
+        for (var i = 0; i < this.tasks.length; i++) {
+          data.push([
+            members1[i],
+            this.tasks[i],
+            members2[i]
+          ]);
+        }
+
+        return data;
+      },
+      set(data) {
+        this.members = data.members;
+        this.tasks = data.tasks;
+        this.teams = data.teams;
+      }
+    }
+  },
+  methods: {
+    subscribeToBack() {
+      Pusher.logToConsole = false;
+      var pusher = new Pusher('3a1d3cf530425138eade', {
+        cluster: 'eu'
+      });
+      var channel = pusher.subscribe('tasks');
+      channel.bind('App\\Events\\StatusTasksUpdated', function(response) {
+        app.table = response.data;
+      });
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.section {
+  border: solid;
 }
 </style>
